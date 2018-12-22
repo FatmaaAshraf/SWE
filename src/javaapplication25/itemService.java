e6949a2850a50aff73781c128ce49cc71a888320
mohamedTarik <31851645+mohamedTarik@users.noreply.github.com>
2018-12-22 22:20:16 +0200

    first commit

---
+++ src/javaapplication25/itemService.java
@@ -0,0 +1,22 @@
+
+package javaapplication25;
+import java.io.*;
+import java.util.*;
+
+public class itemService {
+public void add(String namee,int i)
+  {
+  item e=new item();
+  e.setName(namee,i);
+  
+  }
+  public void remove(item e)
+  {
+  
+  }
+  public void view()
+  {
+  
+  }
+
+}
---
+++ src/javaapplication25/questions.java
@@ -0,0 +1,20 @@
+
+package javaapplication25;
+import java.io.*;
+import java.util.*;
+
+public class questions {
+ private  ArrayList<String>quest=new   ArrayList<String>();
+
+    public ArrayList<String> getQuest() 
+    {
+        return quest;
+    }
+
+    public void setQuest(ArrayList<String> quest)
+    {
+        this.quest = quest;
+    }
+    
+    
+}
