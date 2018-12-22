f80acac461256ba51e352cc92e0ce7ba302c84fd
FatmaaAshraf <fatma_ashraf12@yahoo.com>
2018-12-22 21:02:28 +0200

    first commit

---
+++ src/javaapplication25/questionService.java
@@ -0,0 +1,20 @@
+
+package javaapplication25;
+import java.io.*;
+import java.util.*;
+
+public class questionService {
+  
+    void ask()
+	{
+		
+	}
+	
+	void verify()
+	{
+		
+		
+	}
+    
+    
+}
