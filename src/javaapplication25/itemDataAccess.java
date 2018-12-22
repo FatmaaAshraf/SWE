8272c552d3f1243e125140d97b283fb708dc7960
DiinaSaeed <diinasaeed@yahoo.com>
2018-12-22 21:02:41 +0200

    first commit

---
+++ src/javaapplication25/itemDataAccess.java
@@ -0,0 +1,36 @@
+
+package javaapplication25;
+import java.io.*;
+import java.util.*;
+
+public class itemDataAccess {
+    
+  public ArrayList<item>listOfItem=new ArrayList<item>();
+
+    public void setListOfItem(ArrayList<item> listOfItem) {
+        this.listOfItem = listOfItem;
+    }
+
+    public ArrayList<item> getListOfItem() 
+    {
+        return listOfItem;
+    }
+    public boolean get()
+    {
+    
+        
+        return true;
+    }
+    public void save()
+    {
+    
+    }
+    public void delete()
+    {
+    
+    }
+    public void update()
+    {
+    
+    }
+}
